10fcf13797555492c5a33ef5907c0dcb0e7faf83
f.kemdjeukeng <f.kemdjeukeng@itgstore-consulting.com>
2021-07-29 02:37:46 -0700

    Login Page

--- react-jwt-auth-without-redux/src/services/user.service.js
+++ react-jwt-auth-without-redux/src/services/user.service.js
@@ -9,7 +9,7 @@ class UserService {
     }
 
     getUserBoard() {
-        return axios.get(API_URL + 'user', { headers: authHeader() });
+        return axios.get(API_URL + 'user', { headers: authHeader() }); // Ne peut-on pas mettre le token globallement
     }
 
     getModeratorBoard() {
